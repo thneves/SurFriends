04fa325c5db63c93053ae93f98cac9a1449ef0f7
thneves <tsneves11@gmail.com>
2021-02-04 18:32:29 -0300

    create first controllers, views and routes with testing cases

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home - Surfriends"
+  end
+
+  test 'should get about' do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About - Surfriends"
+  end
+
+end
