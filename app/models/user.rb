c9da8e16e4644244e72361850e342221b3492837
thneves <tsneves11@gmail.com>
2021-02-08 21:40:41 -0300

    create User model, validation for name and email, add tests for model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 10 }
+  VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+end
