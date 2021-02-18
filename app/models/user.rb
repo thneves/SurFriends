5236df06f8d16502628c5ec8d116bdc5f80bf26b
thneves <tsneves11@gmail.com>
2021-02-18 08:04:07 -0300

    update feed feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,9 @@ class User < ApplicationRecord
   has_secure_password
 
   def feed
-    Post.where("user_id = ?", id )
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
