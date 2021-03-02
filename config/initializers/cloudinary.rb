52013f07e711567f086ad475cd0ab194ac8827bf
thneves <tsneves11@gmail.com>
2021-03-02 13:37:42 -0300

    update cloudinary config

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,9 +1,8 @@
 
-
 Cloudinary.config do |config|
-  config.cloud_name = 'sample'
-  config.api_key = '874837483274837'
-  config.api_secret = 'a676b67565c6767a6767d6767f676fe1'
+  config.cloud_name = 'de3vnm3hk'
+  config.api_key = '456146893178241'
+  config.api_secret = 'qYg_NaLjHcBHGYM7zAMXIw2HY0A'
   config.secure = true
   config.cdn_subdomain = true
 end
